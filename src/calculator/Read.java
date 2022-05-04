1205a56911dc64069102d309a2326699e1505daf
Marian Cocolos <cmarianc23@yahoo.com>
2022-05-04 21:05:48 +0300

    Tema 7

---
+++ src/calculator/Read.java
@@ -0,0 +1,72 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Read {
+
+    public int getInt() {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        do {
+            try {
+                System.out.println("Enter a number:");
+                x = scan.nextInt();
+                return x;
+            } catch (InputMismatchException e) {
+                System.err.println("Given value '"+scan.nextLine()+"' is not valid. Please enter a numeric value.");
+            }
+        }while (true);
+    }
+
+    public float getFloat() {
+        Scanner scan = new Scanner(System.in);
+        float x;
+        do {
+            try {
+                System.out.println("Enter a number:");
+                x = scan.nextFloat();
+                return x;
+            } catch (InputMismatchException e) {
+                System.err.println("Given value '"+scan.nextLine()+"' is not valid. Please enter a numeric value.");
+            }
+        }while (true);
+    }
+
+    public double getDouble() {
+        Scanner scan = new Scanner(System.in);
+        double y;
+        do {
+            try {
+                System.out.println("Enter a number:");
+                y = scan.nextDouble();
+                return y;
+            } catch (InputMismatchException e) {
+                System.err.println("Given value '"+scan.nextLine()+"' is not valid. Please enter a numeric value.");
+            }
+        }while (true);
+    }
+
+
+    public List<Integer> getList(){
+        List<Integer> list = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the numbers in the list: ");
+        do {
+            try {
+                list.add(scan.nextInt());
+            }catch (InputMismatchException e){
+                if (scan.nextLine().equalsIgnoreCase("q")){
+                    break;
+                }else {
+                    System.out.println("Invalid value given" );
+                }
+            }
+
+        }while (true);
+        return list;
+    }
+
+}
